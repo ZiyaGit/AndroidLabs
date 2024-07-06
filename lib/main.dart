762ebc3b71c40b1e0f33a68d8d8e7cd2fac9c1aa
Ziya <gule0017@algonquinlive.com>
2024-07-05 22:50:42 -0400

    lab6 done

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: 'Lab 6 Ziya',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
